6b829c38259a3aff4174c4544271561a92e5ade9
Dylan Smith <dylan1999.smith@gmail.com>
2020-04-03 16:04:41 -0500

    base project

--- main.c
+++ main.c
@@ -1,61 +1,90 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <semaphore.h>
 
+int inbuffer_producer = 0;
+int inbuffer_consumer = 0;
+
+int outbuffer_producer = 0;
+int outbuffer_consumer = 0;
+
+int encrypt_position = 0;
+
+
+int buffer_size = 0;
+
 char *input_buffer;
 char *output_buffer;
 
-int buffer_size;
+sem_t inbuffer_used_slots, inbuffer_open_slots, inbuffer_mutex, encrypted, unencrypted, outbuffer_mutex, outbuffer_used_slots, outbuffer_open_slots, outbuffer_count, outbuffer_uncounted;
 
-sem_t inbuffer_mutex, inbuffer_slots;
-
-int producer_position = 0;
-int consumer_position = 0;
-
-void *reader_thread(void *arg)
+void *reader_thread(void *in)
 {
-	FILE *f = fopen(arg, "r");	
+  	FILE *f = fopen(in, "r");
 
-	char c; 
+  	char c;
+ 
+  	do{
+    		c = fgetc(f);
+    
+    		sem_wait(&encrypted);
+    		sem_wait(&inbuffer_open_slots);
+    		sem_wait(&inbuffer_mutex);
 
-	do{
-		c = fgetc(f);
-		sem_wait(&inbuffer_slots);
-		sem_wait(&inbuffer_unencrypted);
-		sem_wait(&inbuffer_mutex);
+    		input_buffer[inbuffer_producer] = c;
+    		inbuffer_producer = (inbuffer_producer + 1) % buffer_size;
+    
+    		sem_post(&inbuffer_mutex);
+    		sem_post(&inbuffer_used_slots);
+    		sem_post(&unencrypted);
+  	}while(c != EOF);
 
-		input_buffer[producer_position] = c;
-		producer_position = (producer_position + 1) % buffer_size;
-
-		sem_post(&inbuffer_mutex);
-		sem_post(&inbuffer_slots);
-	}while(c != EOF);
-
-	pthread_exit(NULL);
+  	pthread_exit(NULL);
 }
 
-void *writer_thread(void *arg)
+void *writer_thread(void *in)
 {
+  	FILE *f = fopen(in, "w");
 
+  	char c;
+  
+  	while(c != EOF){
+    		sem_wait(&outbuffer_used_slots);
+    		sem_wait(&outbuffer_count);
+    		sem_wait(&outbuffer_mutex);
+
+    		c = output_buffer[outbuffer_consumer];
+    		outbuffer_consumer = (outbuffer_consumer + 1) % buffer_size;
+    
+    		sem_post(&outbuffer_mutex);
+    		sem_post(&outbuffer_uncounted);
+    		sem_post(&outbuffer_open_slots);
+    
+    		if(c != EOF) fputc(c, f);
+  	}
+  
+  	pthread_exit(NULL);
 }
 
-void *encryption_thread(void *arg)
+void *encryption_thread(void *in)
 {
-	char c;
-	int s = 1;
+  	char c;
 
-	do{
+  	int s = 1;
+  	do{
+    		sem_wait(&unencrypted);
+    		sem_wait(&inbuffer_mutex);
 
-		sem_wait(&inbuffer_mutex);
+    		c = input_buffer[encrypt_position];
+    		encrypt_position = (encrypt_position + 1) % buffer_size;
 
-		c = input_buffer[producer_position];
-		producer_position = (producer_position + 1) % buffer_size;
+    		sem_post(&inbuffer_mutex);
+    		sem_post(&encrypted);
 
-		sem_post(&inbuffer_mutex);
-
-		if((c > 64 && c < 91) || (c > 96 && c < 122)){
+   		if((c > 64 && c < 91) || (c > 96 && c < 122))
+		{
 			if(s == 1){
 				c++;
 				if(c == 91) c = 65;
@@ -69,57 +98,150 @@ void *encryption_thread(void *arg)
 			} else if(s == 0){
 				s = 1;
 			}
-		}
+		}	
+    
+    		sem_wait(&outbuffer_open_slots);
+    		sem_wait(&outbuffer_mutex);
 
-		
-		output_buffer[consumer_position] = c;
-		consumer_position = (consumer_position + 1) % buffer_size;
+    		output_buffer[outbuffer_producer] = c;
+    		outbuffer_producer = (outbuffer_producer + 1) % buffer_size;
 
-	}while(c != EOF);
+    		sem_post(&outbuffer_mutex);
+    		sem_post(&outbuffer_used_slots);
+  	}while(c != EOF);
+
+  	pthread_exit(NULL);
 }
 
-void *input_thread(void *arg)
+void *input_thread(void *in)
 {
 
+  	char c;
+  	char alphabet[26][1];
+
+  	int i;
+  	for(i = 0; i < 26; i++){
+    		alphabet[i][0] = 0;
+  	}
+  
+  	do{
+    		sem_wait(&inbuffer_used_slots);
+    		sem_wait(&inbuffer_mutex);
+
+    		c = input_buffer[inbuffer_consumer];
+    		inbuffer_consumer = (inbuffer_consumer + 1) % buffer_size;
+
+    		sem_post(&inbuffer_mutex);
+    		sem_post(&inbuffer_open_slots);
+    
+    		if(c > 96 && c < 123){
+      			c = c - 32;
+    		}
+
+    		if(c > 64 && c < 91){
+      			alphabet[c - 65][0]++;
+    		}
+
+
+  	}while(c != EOF);
+
+  	printf("Input file contains\n");
+
+  	for(i = 0; i < 26; i++){
+    		if(alphabet[i][0] != 0){
+      			printf("%c: %d\n", i + 65, alphabet[i][0]);
+    		}
+  	}
+  
+  	pthread_exit(NULL);
+
 }
 
-void *output_thread(void *arg)
+void *output_thread(void *in)
 {
 
-}
+  	char c;
+
+  	char alphabet[26][1];
+
+	int count_position = 0;
+
+  	int i;
+  	for(i = 0; i < 26; i++){
+    		alphabet[i][0] = 0;
+  	}
+  
+  	do{
+    		sem_wait(&outbuffer_uncounted);
+    		sem_wait(&outbuffer_mutex);
+
+    		c = output_buffer[count_position];
+    		count_position = (count_position + 1) % buffer_size;
+
+    		sem_post(&outbuffer_mutex);
+    		sem_post(&outbuffer_count);
+
+    		if(c > 96 && c < 123){
+      			c = c - 32;
+    		}
+
+    		if(c > 64 && c < 91){
+      			alphabet[c - 65][0]++;
+    		}
+
+
+  	}while(c != EOF);
+
+  	printf("Output file contains\n");
+
+  	for(i = 0; i < 26; i++){
+    		if(alphabet[i][0] != 0){
+      			printf("%c: %d\n", i + 65, alphabet[i][0]);
+    		}
+  	}
+  	pthread_exit(NULL);
+} 
+
+
+
+
 
 int main(int argc, char* argv[])
-{ 
-	
-	if(argc != 3)
-	{
-		printf("Usage: ./main [infile] [outfile]\n");	
-		return 0;
-	}
+{
 
-	printf("Enter a buffer size: ");
-	scanf("%d", &buffer_size);
+  	if(argc != 3){
+    		printf("Usage ./main [inputfile] [outputfile]\n");
+      		return 0;
+  	}
 
-	input_buffer = malloc(buffer_size);
-	output_buffer = malloc(buffer_size);
-	
-	sem_init(&inbuffer_mutex, 0, 1);
-	sem_init(&inbuffer_slots, buffer_size);
-	sem_init(&inbuffer_unencrypted, buffer_size);
+  	printf("Enter the buffer size: ");
+  	scanf("%d", &buffer_size);
+  
+  	input_buffer = malloc(buffer_size);
+  	output_buffer = malloc(buffer_size);
 
-	pthread_t p_reader, p_writer, p_encryption, p_input, p_output;
+  	sem_init(&inbuffer_mutex, 0, 1);
+  	sem_init(&inbuffer_used_slots, 0, 0);
+  	sem_init(&unencrypted, 0, 0);
+  	sem_init(&inbuffer_open_slots, 0, buffer_size);
+  	sem_init(&encrypted, 0, buffer_size);
+  	sem_init(&outbuffer_mutex, 0, 1);
+  	sem_init(&outbuffer_used_slots, 0, 0);
+  	sem_init(&outbuffer_open_slots, 0, buffer_size);
+  	sem_init(&outbuffer_count, 0, 0);
+  	sem_init(&outbuffer_uncounted, 0, buffer_size);
+  
+  	pthread_t p_reader, p_writer, p_encryption, p_input, p_output;
 
-	pthread_create(&p_reader, NULL, reader_thread, argv[1]); 
-	int i;
-//	pthread_create(&p_writer, NULL, writer_thread, argv[2]);
-//	pthread_create(&p_encryption, NULL, encryption_thread, NULL);
-//	pthread_create(&p_input, NULL, input_thread, NULL);
-//	pthread_create(&p_output, NULL, output_thread, NULL);
-	
-	pthread_join(p_reader, NULL);
-	for(i=0; i<buffer_size; i++)
-	{
-		printf("%c", input_buffer[i]);
-	}
-  	return 0;
+  	pthread_create(&p_reader, NULL, reader_thread, argv[1]);
+  	pthread_create(&p_writer, NULL, writer_thread, argv[2]);
+  	pthread_create(&p_encryption, NULL, encryption_thread, NULL);
+  	pthread_create(&p_input, NULL, input_thread, NULL);
+  	pthread_create(&p_output, NULL, output_thread, NULL);
+
+  	pthread_join(p_reader, NULL);
+  	pthread_join(p_writer, NULL);
+  	pthread_join(p_encryption, NULL);
+  	pthread_join(p_input, NULL);
+  	pthread_join(p_output, NULL);
 }
